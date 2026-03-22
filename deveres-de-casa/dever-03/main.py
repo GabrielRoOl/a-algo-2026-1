37bbe721688dc78cec531274a12654a4689d5ae1
GabrielRoOl <gabriel.ro.ol@hotmail.com>
2026-03-22 16:05:30 -0300

    feat: add document to code

--- deveres-de-casa/dever-03/main.py
+++ deveres-de-casa/dever-03/main.py
@@ -5,16 +5,22 @@ array4 = ["a", "b", "c", "f", "b", "a"]
 
 print("*"* 50)
 
+# Function recursive to check if an array is a palindrome
 def is_palindrome(array):
+    # Loop to check if the first and last elements are the same
     for i in range(len(array)):
         first = array[i]
         last = array[len(array) - 1 - i]
         
+        # If the first and last elements are not the same, return False
         if first != last:
             return False
 
+    # If the loop finishes without finding any differences, return True
     return True
 
+
+# Check arrays and print the results
 print(f"is_palindrome(array1): {is_palindrome(array1)}")
 print(f"is_palindrome(array2): {is_palindrome(array2)}")
 print(f"is_palindrome(array3): {is_palindrome(array3)}")
